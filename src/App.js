22d266e976af3a44ada2808e6d5beb09341267e6
Melissa <lisastriker@gmail.com>
2021-08-14 16:52:59 +0800

    Did up conditional rendering of dropdown

--- src/App.js
+++ src/App.js
@@ -2,48 +2,41 @@ import logo from './logo.svg';
 import './App.css';
 import { ExpandMore } from '@material-ui/icons';
 import { Accordion, AccordionSummary, AccordionDetails } from '@material-ui/core';
+import { useEffect, useState } from 'react';
+
 function App() {
+
+  const [expandedPanel, setExpandedPanel] = useState(false);
+
+  const handleAccordionChange = (number) => (event, isExpanded) => {
+    console.log({ event, isExpanded });
+    setExpandedPanel(isExpanded ? number : false);
+  };
+
+  useEffect(() => {
+
+  }, []);
+
+  const accordionObject = [
+    { number:1, title:"Hello", detail:"Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse malesuada lacus ex,sit amet blandit leo lobortis eget.testing" }, 
+    { number:2, title:"There", detail:"teLorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse malesuada lacus ex,sit amet blandit leo lobortis eget.ting" },
+    { number:3, title:"You", detail:"Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse malesuada lacus ex,sit amet blandit leo lobortis eget." }
+  ]
+  const listAccordion =  accordionObject.map( data => 
+    <Accordion expanded={expandedPanel === data.number} onChange={handleAccordionChange(data.number)}>
+    <AccordionSummary expandIcon={<ExpandMore />}>{data.title}</AccordionSummary>
+    <AccordionDetails>
+      {data.detail}
+    </AccordionDetails>
+    </Accordion>)
+
+
   return (
     <div className="App">
       <header className="App-header">
-      <Accordion>
-
-<AccordionSummary  expandIcon={<ExpandMore />}>
-Accordion 1
-</AccordionSummary>
-
-<AccordionDetails>
-Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse malesuada lacus ex,
-sit amet blandit leo lobortis eget.
-</AccordionDetails>
-
-</Accordion>
-
-<Accordion>
-
-<AccordionSummary  expandIcon={<ExpandMore />}>
-Accordion 2
-</AccordionSummary>
-
-<AccordionDetails>
-Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse malesuada lacus ex,
-sit amet blandit leo lobortis eget.
-</AccordionDetails>
-
-</Accordion>
-
-<Accordion>
-
-<AccordionSummary  expandIcon={<ExpandMore />}>
-  Accordion 3
-</AccordionSummary>
-
-<AccordionDetails>
-Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse malesuada lacus ex,
-sit amet blandit leo lobortis eget.
-</AccordionDetails>
-
-</Accordion>
+      <div style={{width:"50%"}}>
+      {listAccordion} 
+      </div>
       </header>
     </div>
   );
