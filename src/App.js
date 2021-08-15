810c5103f1d794ed0d0fad9ecfc2245fbf8d821e
Melissa <lisastriker@gmail.com>
2021-08-15 12:26:29 +0800

    Added pagination

--- src/App.js
+++ src/App.js
@@ -5,20 +5,20 @@ import { Accordion, AccordionSummary, AccordionDetails } from '@material-ui/core
 import { useEffect, useState } from 'react';
 import { firebaseConfig } from "./firebaseConfig"
 import firebase from 'firebase/app';
-
-// Required for side-effects
-
-// import "firebase/autconst firebase = require("firebase");
-// Required for side-effects
 import "firebase/firestore";
+import { Pagination } from '@material-ui/lab'
 
 firebase.initializeApp(firebaseConfig);
 var db = firebase.firestore()
 
 function App() {
+  const [loaded, setLoaded] = useState(false)
   const [firebaseData, setFirebaseData] = useState([""])
   const [expandedPanel, setExpandedPanel] = useState(false);
-  const [loaded, setLoaded] = useState(false)
+  const [currentPage, setCurrentPage] = useState(1);
+  const [postsPerPage] = useState(10);
+  const [pageNumber] = useState()
+  const [page, setPage] = useState(1)
   const handleAccordionChange = (number) => (event, isExpanded) => {
     console.log({ event, isExpanded });
     setExpandedPanel(isExpanded ? number : false);
@@ -42,28 +42,47 @@ function App() {
 
   useEffect(() => {
     parseData(db)
+
   }, []);
+
+  const handleChange = (event, value) => {
+    setPage(value);
+  };
   
+  // const paginate = (page) => {
+  //   setLoaded(true) 
+  //   setCurrentPage(page);
+  // }
+  const indexOfLastPost = page * postsPerPage;
+  const indexOfFirstPost = indexOfLastPost - postsPerPage;
   const accordionObject = [firebaseData][0]
   console.log(accordionObject)
-  const listAccordion =  accordionObject.map( data => 
+  const slice = accordionObject.slice(indexOfFirstPost, indexOfLastPost)
+  const listAccordian = slice.map(data => {
+    return(        
     <Accordion expanded={expandedPanel === data.id} onChange={handleAccordionChange(data.id)}>
     <AccordionSummary expandIcon={<ExpandMore />}>{data.Subject}</AccordionSummary>
     <AccordionDetails style={{display:"flex", "word-break":"break-word"}}>
       {data.Body}
     </AccordionDetails>
     </Accordion>)
-
+  }
+  )
 
   return (
     <div className="App">
       <header className="App-header">
       <div style={{width:"50%"}}>
-      {loaded ? listAccordion : null} 
+      {loaded ? listAccordian : null} 
+      <Pagination color='primary' page={page} count={Math.ceil(firebaseData.length / postsPerPage)} onChange={handleChange}/>
       </div>
+      
       </header>
     </div>
   );
 }
 
 export default App;
+// <Pagination postsPerPage={postsPerPage}
+// totalPosts={firebaseData.length}
+// paginate={paginate}/>
